9e8828bd2dcf344fc488994e3809baa3ed990307
Fritjof Aufschlaeger <34543332+LeFrit@users.noreply.github.com>
2023-09-30 23:39:31 +0200

    type annotation updated

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -1,3 +1,4 @@
+from flask.testing import FlaskClient
 import pytest
 from src.app import app
 
@@ -7,7 +8,7 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_index_route(client):
+def test_index_route(client: FlaskClient):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello World!' in response.data
