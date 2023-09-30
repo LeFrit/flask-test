debe384cdcc7189f0fe5b992c1580662b7b7a043
Fritjof Aufschlaeger <34543332+LeFrit@users.noreply.github.com>
2023-09-30 19:31:38 +0200

    initial commit

---
+++ server/src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+import time
+
+app = Flask(__name__)
+
+def format_time():
+    local_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", local_time)
+
+@app.route('/')
+def index():
+    context = {
+        "local_time": format_time()
+    }
+    return render_template('index.html', context=context)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
